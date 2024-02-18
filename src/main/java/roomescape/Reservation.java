16a36430ec1e5fd002ab8081af79171a31a0fad8
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-02-18 22:34:11 +0900

    Remove duplicates

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -11,12 +11,4 @@ public class Reservation {
     private String name;
     private String date;
     private String time;
-
-    @Builder
-    public Reservation(Long id, String name, String date, String time) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
 }
