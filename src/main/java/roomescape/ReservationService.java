8839134c2b29819ed5c68eef6cde9fa9877a6ac2
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-02-18 22:15:54 +0900

    add exception

--- src/main/java/roomescape/ReservationService.java
+++ src/main/java/roomescape/ReservationService.java
@@ -2,6 +2,10 @@ package roomescape;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import roomescape.Exception.ParamException;
+import roomescape.Exception.ReservationException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
@@ -16,6 +20,11 @@ public class ReservationService {
         return reservations;
     }
     public Reservation addReservation(Reservation reservation) {
+
+        if (StringUtils.isEmpty(reservation.getName()) || StringUtils.isEmpty(reservation.getDate()) || StringUtils.isEmpty(reservation.getTime())) {
+            throw new ParamException(ParamException.Type.MISSING_PARAMETER);
+        }
+
         Reservation newReservation = Reservation.builder()
                 .id(index.incrementAndGet())
                 .name(reservation.getName())
@@ -29,6 +38,11 @@ public class ReservationService {
     }
 
     public void deleteReservation(Long id) {
+        // 삭제할 예약이 없을 때 예외처리
+        if (reservations.stream().noneMatch(reservation -> reservation.getId().equals(id))) {
+            throw new ReservationException(ReservationException.Type.NOT_FOUND_RESERVATION);
+        }
+
         reservations.removeIf(reservation -> reservation.getId().equals(id));
     }
 }
