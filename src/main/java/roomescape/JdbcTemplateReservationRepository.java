bbf0a537af821a4fd3a62929fce0c56922ffac27
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-02-23 21:12:41 +0900

    add repository

--- src/main/java/roomescape/JdbcTemplateReservationRepository.java
+++ src/main/java/roomescape/JdbcTemplateReservationRepository.java
@@ -2,9 +2,12 @@ package roomescape;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
@@ -19,13 +22,35 @@ public class JdbcTemplateReservationRepository {
         return jdbcTemplate.query("select * from reservation", reservationRowMapper);
     }
 
+    public Long insertAndGetGeneratedKey(Reservation reservation) {
+        String sql = "insert into reservation (name, date, time) values (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    sql, new String[]{"id"});
+            ps.setString(1, reservation.getName());
+            ps.setString(2, reservation.getDate());
+            ps.setString(3, reservation.getTime());
+            return ps;
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
+    }
+
+    public boolean delete(Long id) {
+        String sql = "delete from reservation where id = ?";
+
+        // 삭제에 성공하면 1반환 실패하면 0 반환
+        int isDeleted = jdbcTemplate.update(sql, Long.valueOf(id));
+        return isDeleted == 1;
+    }
+
     private final RowMapper<Reservation> reservationRowMapper = (rs, rowNum) -> {
-            Reservation reservation = new Reservation(
-                    rs.getLong("id"),
-                    rs.getString("name"),
-                    rs.getString("date"),
-                    rs.getString("time")
-            );
-            return reservation;
+        Reservation reservation = new Reservation(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getString("date"),
+                rs.getString("time")
+        );
+        return reservation;
     };
 }
